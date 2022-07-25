1da877eaf07233384b2b52b3d6bbbd845bfa454f
silvio <silvioromano.dev@gmail.com>
2022-07-25 07:38:14 -0300

    Implementing findById

--- src/model/dao/DepartmentDao.java
+++ src/model/dao/DepartmentDao.java
@@ -8,6 +8,6 @@ public interface DepartmentDao {
 	void insert (Department obj);
 	void update (Department obj);
 	void deleteById (Department obj);
-	Department findById (Department obj);
+	Department findById (Integer id);
 	List<Department> findAll();
 }
--- src/model/dao/SellerDao.java
+++ src/model/dao/SellerDao.java
@@ -8,6 +8,6 @@ public interface SellerDao {
 	void insert (Seller obj);
 	void update (Seller obj);
 	void deleteById (Seller obj);
-	Seller findById (Seller obj);
+	Seller findById (Integer id);
 	List<Seller> findAll();
 }
